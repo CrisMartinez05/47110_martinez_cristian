5c8abc4c5949e0d5e18e5ba4106a0ed7170e097c
Cristian Martinez <mrkittmm@gmail.com>
2023-11-03 10:57:18 -0300

    Agregué un bucle para evitar calcular el requerimiento calorico en menores de 9 años

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const punto = ".";
 const blanco = " ";
-let nombreUsuario = prompt("Por favor ingrese su nombre.");
+let nombreUsuario;
 let peso = 0;
 let edad = 0;
 let nivelActividad;
@@ -10,60 +10,62 @@ let reqCalorico;
 function imbHombre() {
     if (edad > 60) {
         resultadoImb = peso * 13.5 + 487;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else if (edad > 30) {
         resultadoImb = peso * 11.6 + 879;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else if (edad > 18) {
         resultadoImb = peso * 15.3 + 679;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else if (edad > 9) {
         resultadoImb = peso * 17.5 + 651;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else {
         alert("Su metabolismo basal no se debe calcular.");
     }
+
     nivelActividad = prompt("Indique su nivel de actividad: \n 1 generalmente inactivo (Sentado), \n 2 levemente activo(Caminatas), \n 3 moderadamente activo (Ejercicio 2 a 3 veces por semana), \n 4 Activo (Ejerc. más de 3 veces por semana), \n 5 Muy activo (Ejerc. todos los días)");
     switch (nivelActividad) {
         case "1":
             reqCalorico = resultadoImb * 1.2;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "2":
             reqCalorico = resultadoImb * 1.3;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "3":
             reqCalorico = resultadoImb * 1.4;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "4":
             reqCalorico = resultadoImb * 1.5;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "5":
             reqCalorico = resultadoImb * 1.7;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         default:
             alert("Digite una opción válida.");
             break;
     }
+
 }
 
 function imbMujer() {
     if (edad > 60) {
         resultadoImb = peso * 10.5 + 596;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else if (edad > 30) {
         resultadoImb = peso * 8.7 + 829;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else if (edad > 18) {
         resultadoImb = peso * 14.7 + 479;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else if (edad > 9) {
         resultadoImb = peso * 12.2 + 746;
-        alert("Su metabolismo basal es de" + blanco + resultadoImb + blanco + "calorías" + punto);
+        alert("Su metabolismo basal es de" + blanco + resultadoImb.toFixed() + blanco + "calorías" + punto);
     } else {
         alert("Su metabolismo basal no se debe calcular.");
     }
@@ -71,23 +73,23 @@ function imbMujer() {
     switch (nivelActividad) {
         case "1":
             reqCalorico = resultadoImb * 1.2;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "2":
             reqCalorico = resultadoImb * 1.3;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "3":
             reqCalorico = resultadoImb * 1.4;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "4":
             reqCalorico = resultadoImb * 1.5;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         case "5":
             reqCalorico = resultadoImb * 1.7;
-            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico + blanco + "calorías al día" + punto);
+            alert("Según su nivel de actividad declarado debería consumir" + blanco + reqCalorico.toFixed() + blanco + "calorías al día" + punto);
             break;
         default:
             alert("Digite una opción válida.");
@@ -95,23 +97,38 @@ function imbMujer() {
     }
 }
 
-class Usuario{
-    constructor(nombre, edad, peso){
-        this.nombre=nombre;
-        this.edad=edad;
-        this.peso=parseFloat(peso);
+class Usuario {
+    constructor(nombre, edad, peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = parseFloat(peso);
     }
 }
 
+nombreUsuario = prompt("Por favor ingrese su nombre.");
 
 while (nombreUsuario == "") {
     alert("Debe ingresar un nombre válido.");
+    
     nombreUsuario = prompt("Por favor ingrese su nombre.");
 }
 alert("Buenos días Sr/a " + nombreUsuario + punto);
 
 edad = parseInt(prompt("Ingrese su edad."));
 
+while(edad<9){
+    alert("Su metabolismo basal no se debe calcular.");
+    alert("Hasta luego.");
+
+    nombreUsuario = prompt("Por favor ingrese su nombre.");
+    while (nombreUsuario == "") {
+        alert("Debe ingresar un nombre válido.");
+        nombreUsuario = prompt("Por favor ingrese su nombre.");
+    }
+    alert("Buenos días Sr/a " + nombreUsuario + punto);
+    
+    edad = parseInt(prompt("Ingrese su edad."));
+}
 alert("La edad ingresada es" + blanco + edad + blanco + "años" + punto);
 
 peso = parseInt(prompt("Ahora ingrese su peso en kilos."));
